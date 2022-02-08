c58a350fab8534a534ef1016651440d2a6ae4c4f
olayode <aolayode@getcarbon.co>
2022-02-08 11:59:13 +0100

    project setup

---
+++ src/main/java/com/possible/springintegrationtut/SpringIntegrationTutApplication.java
@@ -0,0 +1,13 @@
+package com.possible.springintegrationtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegrationTutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/possible/springintegrationtut/SpringIntegrationTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.possible.springintegrationtut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
